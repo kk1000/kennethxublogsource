effffb9b9814e2ec90b9cff861b0ae3ca1d695f4
kennethxu <kennethxu@sharneng.com>
2009-04-11 05:01:18 +0000

    extension method support

---
+++ Net20ExtensionMethodSupport/Net20ExtensionMethodSupport/ExtensionAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// This allows us to use extension method when targeted to 2.0
+    /// </summary>
+    public class ExtensionAttribute : Attribute
+    {
+    }
+}
