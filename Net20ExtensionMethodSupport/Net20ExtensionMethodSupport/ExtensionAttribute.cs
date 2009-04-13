ee14a8b758fa5a9017ceca06b574ef25ddd02e78
kennethxu <kennethxu@sharneng.com>
2009-04-13 06:34:09 +0000

    Delete Net20ExtensionMethodSupport project which is replaced by LinqBridge, also an open source lib.

--- Net20ExtensionMethodSupport/Net20ExtensionMethodSupport/ExtensionAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    /// <summary>
-    /// This allows us to use extension method when targeted to 2.0
-    /// </summary>
-    public class ExtensionAttribute : Attribute
-    {
-    }
-}
