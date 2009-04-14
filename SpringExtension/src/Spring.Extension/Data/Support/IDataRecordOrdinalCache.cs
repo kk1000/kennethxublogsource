5e994361ea422b34611ee81c71545f902f9907d0
kennethxu <kennethxu@sharneng.com>
2009-04-14 00:33:26 +0000

    SpringExtension: Redesign of the cache for IDataRecord ordinal lookup.

---
+++ SpringExtension/src/Spring.Extension/Data/Support/IDataRecordOrdinalCache.cs
@@ -0,0 +1,52 @@
+﻿#region License
+
+/*
+ * Copyright (C) 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#endregion
+
+using System.Data;
+
+namespace Spring.Data.Support
+{
+    /// <summary>
+    /// Defines the operation of field name to ordinal cache to improve the
+    /// performance of retrieve data from <see cref="IDataRecord"/> by field
+    /// name. Implementation must be thread safe.
+    /// </summary>
+    /// <author>Kenneth Xu</author>
+    public interface IDataRecordOrdinalCache
+    {
+        /// <summary>
+        /// Initialize the cache. Must be called at least once before any other
+        /// method can be called.
+        /// </summary>
+        /// <remarks>
+        /// First call will build the cache. Subsequent calls are ignored.
+        /// </remarks>
+        /// <param name="dataRecord">
+        /// The <see cref="IDataRecord"/> object used to initialize the cache.
+        /// </param>
+        void Init(IDataRecord dataRecord);
+
+        /// <summary>
+        /// Get the ordinal of field with given <paramref name="name"/>
+        /// </summary>
+        /// <param name="name">The name of the field.</param>
+        /// <returns>The cached ordinal of the field.</returns>
+        int GetOrdinal(string name);
+    }
+}
---
+++ SpringExtension/test/Spring.Extension.Tests/Data/Support/DataRecordOrdinalCacheTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+using Spring.Data.Support;
+
+namespace Spring.Extension.Tests.Data.Support
+{
+    [TestFixture] public class DataRecordOrdinalCacheTest
+    {
+        private MockRepository _mockery;
+        private DataRecordOrdinalCache _testee;
+        private IDataReader _dataReader;
+
+        private static readonly string[] _fieldNames = new string[]{"lower_case", "UPPERCASE", "CamelCase"};
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockery = new MockRepository();
+            _testee = new DataRecordOrdinalCache();
+            _dataReader = _mockery.CreateMock<IDataReader>();
+
+            Expect.Call(_dataReader.FieldCount).Return(_fieldNames.Length);
+            for (int i = 0; i < _fieldNames.Length; i++)
+            {
+                Expect.Call(_dataReader.GetName(i)).Return(_fieldNames[i]);
+            }
+        }
+
+        [Test] public void InitChokesOnNullDataRecord()
+        {
+            TestHelper.AssertException<ArgumentNullException>(
+                () => _testee.Init(null),
+                MessageMatch.Contains,
+                "dataRecord"
+                );
+        }
+
+        [Test] public void InitCachesOnFirstCall()
+        {
+            _mockery.ReplayAll();
+            _testee.Init(_dataReader);
+            _mockery.VerifyAll();
+        }
+
+        [Test] public void InitIgnoresSubsequentCall()
+        {
+            _mockery.ReplayAll();
+            _testee.Init(_dataReader);
+            _testee.Init(_dataReader); // second call
+            _mockery.VerifyAll();
+        }
+
+        [Test] public void GetOridnalChokesBeforeInit()
+        {
+            TestHelper.AssertException<InvalidOperationException>(
+                ()=>_testee.GetOrdinal("any"));
+        }
+
+        [Test] public void GetOridnalChokesWhenNameDoesNotExist()
+        {
+            _mockery.ReplayAll();
+            const string nameDoesNotExist = "nameDoesNotExist";
+            _testee.Init(_dataReader);
+            TestHelper.AssertException<IndexOutOfRangeException>(
+                ()=>_testee.GetOrdinal(nameDoesNotExist),
+                MessageMatch.Contains,
+                nameDoesNotExist);
+        }
+
+        [Test] public void GetOrdinalMatchesExactName()
+        {
+            _mockery.ReplayAll();
+            _testee.Init(_dataReader);
+            for (int i = 0; i < _fieldNames.Length; i++)
+            {
+                Assert.That(_testee.GetOrdinal(_fieldNames[i]), Iz.EqualTo(i));
+            }
+        }
+
+        [Test] public void GetOrinalCaseInsensitive()
+        {
+            _mockery.ReplayAll();
+            _testee.Init(_dataReader);
+            Assert.That(_testee.GetOrdinal("LOWER_CASE"), Iz.EqualTo(0));
+            Assert.That(_testee.GetOrdinal("Lower_Case"), Iz.EqualTo(0));
+            Assert.That(_testee.GetOrdinal("uppercase"), Iz.EqualTo(1));
+            Assert.That(_testee.GetOrdinal("CAMELCASE"), Iz.EqualTo(2));
+            Assert.That(_testee.GetOrdinal("cAMELcASE"), Iz.EqualTo(2));
+        }
+    }
+}
