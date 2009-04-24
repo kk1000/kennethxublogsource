d39d0e4721abe0b32b666dd6d6740bcffc239e4e
kennethx <kennethx@6564fcb6-2645-11de-b990-bf4a61c80bdd>
2009-04-24 15:52:03 +0000

    SpringExtension: change version number to 1.1.0

--- SpringExtension/AssemblyVersion.cs
+++ SpringExtension/AssemblyVersion.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("1.0.0.*")]
+[assembly: AssemblyVersionAttribute("1.1.0.*")]
 [assembly: AssemblyCompanyAttribute("")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2009")]
 
