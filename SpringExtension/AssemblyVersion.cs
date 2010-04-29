adc67d05d53abf638f5f71be7c154d3c37ddcbfc
kennethx <kennethx@6564fcb6-2645-11de-b990-bf4a61c80bdd>
2010-04-29 00:22:54 +0000

    SpringExtension: set version number to 1.3.1

--- SpringExtension/AssemblyVersion.cs
+++ SpringExtension/AssemblyVersion.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("1.3.0.*")]
+[assembly: AssemblyVersionAttribute("1.3.1.*")]
 
