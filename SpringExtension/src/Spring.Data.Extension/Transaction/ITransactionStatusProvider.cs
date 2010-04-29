8ab1679bd71fee6d6f73ce8379d2f98bd1dc1f3f
kennethx <kennethx@6564fcb6-2645-11de-b990-bf4a61c80bdd>
2010-04-29 00:14:43 +0000

    SpringExtension: add implementation for ITransactionStatusProvider.

---
+++ SpringExtension/src/Spring.Data.Extension/Transaction/ITransactionStatusProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Spring.Transaction
+{
+    /// <summary>
+    /// An interface to query for the current transaction status.
+    /// mainly intended for code that wants to set the current transaction
+    /// rollback-only but not throw an application exception.
+    /// </summary>
+    /// <remarks>
+    /// This is useful to make your unit testable by replacing
+    /// <code>TransactionInterceptor.CurrentTransactionStatus.SetRollbackOnly();</code>
+    /// with an injectable <see cref="ITransactionStatusProvider"/> instance.
+    /// </remarks>
+    public interface ITransactionStatusProvider
+    {
+        /// <summary>
+        /// Returns the <see cref="ITransactionStatus"/> of the current method invocation.
+        /// </summary>
+        /// <exception cref="NoTransactionException">
+        /// If the transaction info cannot be found.
+        /// </exception>
+        ITransactionStatus CurrentTransactionStatus { get; }
+    }
+}
---
+++ SpringExtension/src/Spring.Data.Extension/Transaction/Interceptor/TransactionInterceptorStatusProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace Spring.Transaction.Interceptor
+{
+    /// <summary>
+    /// An <see cref="ITransactionStatusProvider"/> that obtains the current
+    /// transaction status created by by declarative transaction management.
+    /// </summary>
+    public class TransactionInterceptorStatusProvider : ITransactionStatusProvider
+    {
+        /// <summary>
+        /// Returns the <see cref="ITransactionStatus"/> of the current method invocation.
+        /// </summary>
+        /// <exception cref="NoTransactionException">
+        /// If the transaction info cannot be found, because the method was
+        /// invoked outside of an AOP invocation context.
+        /// </exception>
+        public ITransactionStatus CurrentTransactionStatus
+        {
+            get { return TransactionInterceptor.CurrentTransactionStatus; }
+        }
+    }
+}
